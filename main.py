1ece44038c4306094d901c79621dfbd8ce4504f7
IAKSH <1248675212@qq.com>
2022-10-15 19:58:25 +0800

    better code

--- main.py
+++ main.py
@@ -6,38 +6,34 @@ import time
 import re
 import os
 
-count = 0
-while(count < 3):
+def getHtml(url):
     pool = urllib3.PoolManager()
-    #res = pool.request("GET","https://www.backweb.org.cn/web/202209/15/http://www.people.com.cn/")
-    res = pool.request("GET","http://www.people.com.cn/")
-    #entrance = re.search("<h1 class=\"fbold\" id=\"rm_topline\"><a href=\"(.*?)\"",res.data.decode())
-    entrance = re.search("<h1 class=\"fbold\" id=\"rm_topline\"><a href=\"(.*?)\"",res.data.decode("gb2312"))
-    entrance2 = re.findall("\"(.*?)\"",entrance.group())
-    print("解析到头条文章链接：" + entrance2[2])
+    homepageRes = pool.request("GET",url)
+    headlineURLRegular  = re.findall("\"(.*?)\"",re.search("<h1 class=\"fbold\" id=\"rm_topline\"><a href=\"(.*?)\"",homepageRes.data.decode("gb2312")).group())
+    print("解析到头条文章链接：" + headlineURLRegular[2])
+    headlineResRegular = pool.request("GET",headlineURLRegular[2])
+    headLineHTMLStr = headlineResRegular.data.decode("gbk")
+    headLineHTMLStr.encode()#转utf-8
+    return headLineHTMLStr
 
-    res2 = pool.request("GET",entrance2[2])
-    headLineHTML = res2.data.decode("gbk")
-    headLineHTML.encode()#转utf-8
+def fixText(text):
+    text = text.replace("<p>","")
+    text = text.replace("</p>","")
+    text = text.replace("</p>","")
+    text = text.replace("<strong>","")
+    text = text.replace("</strong>","")
+    text = text.replace("<p style=\"text-indent: 2em;\">","")
+    text = text.replace("<p style=\"text-align: center;\">","")
+    text = text.replace("<script src=\"http://tv.people.com.cn/img/player/v.js\"></script><script>showPlayer({id:\"/pvservice/xml/2022/10/13/91067ddb-857a-4bcc-b6e4-c9323326c7d5.xml\",width:640,height:360,skin:2});</script>","")
+    return text
 
-    content = re.findall("<p>\n(.*?)</p>",headLineHTML)# 这里还是不行，它们的html一点都不规范，有时候换行有时候不换，有时候还把p大写
-    print(content)
-    with open("all.txt",mode="a",encoding="utf-8") as allFile:
-        with open(time.strftime('%Y-%m-%d', time.localtime()) + ".txt",mode="a",encoding="utf-8") as file:
-            for i in content:
-                line = i.replace("&nbsp;&nbsp;","\n").replace("</strong>","").replace("<strong>","").replace("<span id=\"paper_num\">　　","").replace("</span>","").replace("	　　","") + "\n"        
-                print("saving line: " + line)
-                file.write(line)
-                allFile.write(line)
-    
-    # 生成词云
-    # 临时放在这里
+def showWordCloud():
     allText = ""
     with open("all.txt",mode="r",encoding="utf-8") as file:
         lines = file.readlines()
         for i in lines:
             allText += i
-    
+        
     cuts = jieba.lcut(allText)
     wordDic = {}
     for i in cuts:
@@ -45,7 +41,8 @@ while(count < 3):
             wordDic[i] += 1
         else:
             wordDic[i] = 0
-    wordDic.pop("\n")
+    if "\n" in wordDic.keys():
+        wordDic.pop("\n")
 
     wordList = []
     for i in wordDic:
@@ -60,10 +57,27 @@ while(count < 3):
     plt.axis('off')
     plt.show()
 
-    print("work done, see you next day.")
-    while(True):
-        time.sleep(10)
-        if time.strftime("%H:%M:%S") == "20:00:00" or input() == "n":
-            count += 1
-            break
+cycleCount = 0
+while cycleCount != 3:
+    headlineLines = re.findall("(.*?)\n",getHtml("http://www.people.com.cn/"))
+    with open("all.txt",mode="a",encoding="utf-8") as globalWordsFile:
+        with open(time.strftime("%Y-%m-%d") + ".txt",mode="a",encoding="utf-8") as file:
+            singnal = 0
+            for line in headlineLines:
+                line += '\n'
+                line = fixText(line)
+                if "<div class=\"box_pic\"></div>" in line:
+                    singnal = 1
+                    continue
+                if singnal == 1:
+                    if "<div class=\"zdfy clearfix\"></div><center><table border=\"0\" align=\"center\" width=\"40%\"><tr></tr></table></center>" in line:
+                        singnal = 0
+                        break
+                    file.write(line)
+                    globalWordsFile.write(line)
+    showWordCloud()
 
+    print("see you net day.")
+    while not time.strftime("%H:%M:%S") == "20:00:00" or input() == "n":
+        time.sleep(100)
+    cycleCount += 1
